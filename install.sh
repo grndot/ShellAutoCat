10d836fc43e16ff2183963cb3bf95ee22755a051
Kazimir <pankazimierzwlesie@protonmail.com>
2023-03-02 23:45:25 +0100

    script is done

--- install.sh
+++ install.sh
@@ -6,20 +6,21 @@ read filename
 echo 'Which shell (sh, bash, zsh, fish and etc.) does this terminal use?'
 read shell
 
-echo '''
-
+echo "
 ### Start Of ShellAutoCat ###
 
-
 function cd() {
-    builtin cd "$@" 
-    if [ -f "${filename}" ]; then
-        cat ${filename}
+    builtin cd \"\$@\" 
+    if [ -f $filename ]; then
+        cat $filename
     fi
 }
 
 export cd
 
-### End Of ShellAutoCat ###
+if [ -f $filename ]; then
+    cat $filename
+fi
 
-''' >> ~/.${shell}rc
+### End Of ShellAutoCat ###
+" >> ~/.${shell}rc
